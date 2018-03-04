6bdfe476bb24346e31db1758fe51464e9636eafa
Marcin <cimon87@gmail.com>
2018-03-04 21:19:54 +0100

    added add button

--- pages/permissions/index.vue
+++ pages/permissions/index.vue
@@ -61,6 +61,19 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
+    <br>
+    <v-fab-transition>
+      <v-btn
+        color="green"
+        dark
+        fixed
+        fab
+        bottom
+        left
+      >
+        <v-icon>add</v-icon>
+      </v-btn>
+    </v-fab-transition>
   </v-container>
 </template>
 

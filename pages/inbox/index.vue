34894dfc29dd9c214f39067b38c5bf302eb8f7cd
Marcin Radziewicz <marcin.radziewicz@ttms.pl>
2018-05-22 15:59:56 +0200

    Added checkbox to delete inbox

--- pages/inbox/index.vue
+++ pages/inbox/index.vue
@@ -6,14 +6,33 @@
           :headers="headers"
           :items="inboxList"
           :loading="isLoading"
+           v-model="selected"
           :pagination.sync="pagination"
+          select-all
           class="elevation-1"
   >
   <v-progress-linear slot="progress" color="blue"  indeterminate></v-progress-linear>
+   <template slot="headerCell" slot-scope="props">
+      <v-tooltip bottom>
+        <span slot="activator">
+          {{ props.header.text }}
+        </span>
+        <span>
+          {{ props.header.text }}
+        </span>
+      </v-tooltip>
+    </template>
     <template slot="items" slot-scope="props">
-      <td width='10%'>{{ props.item.SenderNumber }}</td>
-      <td width='75%' class="text-xs-left">{{ props.item.TextDecoded }}</td>
-      <td class="text-xs-left">{{ formatDate(props.item.ReceivingDateTime) }}</td>
+        <td>
+          <v-checkbox
+            v-model="props.selected"
+            primary
+            hide-details
+          ></v-checkbox>
+        </td>
+        <td width='10%'>{{ props.item.SenderNumber }}</td>
+        <td width='75%' class="text-xs-left">{{ props.item.TextDecoded }}</td>
+        <td class="text-xs-left">{{ formatDate(props.item.ReceivingDateTime) }}</td>
     </template>
   </v-data-table>
       </v-flex>
@@ -28,6 +47,7 @@ import TableFormatter from '@/js/formatters/TableFormatter'
 export default {
   data () {
     return {
+      selected: [],
       pagination: {
         sortBy: 'ID',
         descending : true,
@@ -69,7 +89,19 @@ export default {
         console.log(err);
         this.errorMesssage = err.Message;
       })
-    }
+    },
+    toggleAll () {
+        if (this.selected.length) this.selected = []
+        else this.selected = this.inboxList.slice()
+    },
+    changeSort (column) {
+        if (this.pagination.sortBy === column) {
+          this.pagination.descending = !this.pagination.descending
+        } else {
+          this.pagination.sortBy = column
+          this.pagination.descending = false
+        }
+      }
   },
 }
 </script>
